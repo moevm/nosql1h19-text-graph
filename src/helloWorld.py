f6a0e4f9b3bd4325eebb155dedf3ce56731edf15
ierikhon <kinglordlear@gmail.com>
2019-02-19 17:43:36 +0300

    Hello world node creation

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -9,6 +9,20 @@ class HelloWorldExample(object):
     def close(self):
         self._driver.close()
 
+    def print_greeting(self, message):
+        with self._driver.session() as session:
+            greeting = session.write_transaction(self._create_and_return_greeting, message)
+            print(greeting)
 
-database = HelloWorldExample('http://localhost:7474/', 'neo4j', 'neo4j')
+    @staticmethod
+    def _create_and_return_greeting(tx, message):
+        result = tx.run("CREATE (a:Greeting) "
+                        "SET a.message = $message "
+                        "RETURN a.message + ', from node ' + id(a)", message=message)
+        return result.single()[0]
+
+
+database = HelloWorldExample('bolt://localhost:7687', 'neo4j', 'kinix951')
+database.print_greeting('Hello')
+database.print_greeting('world')
 database.close()
