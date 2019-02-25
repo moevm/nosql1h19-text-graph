29914bcff6672b6ae93f3ed0580677e525aca1a7
SqrtMinusOne <thexcloud@gmail.com>
2019-02-25 10:51:09 +0300

    Conditional mkdir in buildUIs

--- buildUI.bat
+++ buildUI.bat
@@ -1,6 +1,11 @@
-call venv/Scripts/activate
-rm -rf src/ui/*
+call venv\Scripts\activate
+IF EXIST src\ui (
+    rm -rf src\ui\*
+) ELSE (
+    md src\ui
+)
 FOR %%I in (ui\*.*) DO (
 	echo Converting %%I
-	call pyuic5 %%I >> src/ui/%%~nI.py
+	call pyuic5 %%I >> src\ui\%%~nI.py
 )
+echo Finished
--- buildUI.sh
+++ buildUI.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 source venv/bin/activate;
+if ! [[ -d src/ui ]]; then
+    mkdir src/ui;
+fi
 rm -rf src/ui/*;
-#mkdir src/ui;
 for filename in ui/*.ui; do
     echo Converting ${filename:3:-3}.py;
     pyuic5 ${filename} >> src/ui/${filename:3:-3}.py;
