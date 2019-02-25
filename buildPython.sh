76a68faef88c1761c0e333339c837af05494f6b3
SqrtMinusOne <thexcloud@gmail.com>
2019-02-25 12:08:45 +0300

    PyInstaller

---
+++ buildPython.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+source venv/bin/activate;
+if [[ -d build ]]; then
+    rm -rf build
+fi
+if [[ -d dist ]]; then
+    rm -rf dist
+fi
+pyinstaller src/main.py --paths=src/:src/config/:src/models/:src/ui/ --additional-hooks-dir=hooks/
