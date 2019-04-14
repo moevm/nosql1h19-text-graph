124fad20a0af44da15108c6552a61b4bd54339cc
SqrtMinusOne <thexcloud@gmail.com>
2019-04-14 22:16:55 +0300

    TextItem

--- src/ui/graph/__init__.py
+++ src/ui/graph/__init__.py
@@ -1,4 +1,5 @@
 from .edge import AnimatedObject, Edge
 from .graphwidget import GraphWidget
 from .node import Node
+from .text_item import TextItem
 from .graphmodule import GraphModule
--- src/ui/graph/graphmodule.py
+++ src/ui/graph/graphmodule.py
@@ -1,8 +1,9 @@
-from ui.graph import Node, Edge, GraphWidget
+from ui.graph import Node, Edge, GraphWidget, TextItem
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QMainWindow
 import numpy as np
 import sys
+import re
 
 
 class GraphModule:
@@ -30,8 +31,28 @@ class GraphModule:
 
         self._nodes = {}  # Хранилище вершин
         self._edges = {}  # Хранилище связей
+        self._texts = {}  # Хранилище текстов
 
         self.gravity_timer = None  # Таймер для работы с гравитацией
+
+    def add_text(self, id, pos_x, pos_y, parent_id=None, **kwargs):
+        """Добавить текст
+
+        :param id: Уникальный id текста
+        :param pos_x: x-координата
+        :param pos_y: y-координата
+        :param **kwargs: Аргументы конструктора TextItem
+        """
+        assert id not in self._texts
+        try:
+            parent_node = self._nodes[parent_id]
+        except KeyError:
+            parent_node = None
+        text = TextItem(id=id, parent=parent_node, **kwargs)
+        text.setPos(pos_x, pos_y)
+        text.linkActivated.connect(self._on_text_link_clicked)
+        self._texts[id] = text
+        self.widget.scene().addItem(text)
 
     def add_node(self, id, pos_x, pos_y, **kwargs):
         """Добавить вершину
@@ -41,6 +62,7 @@ class GraphModule:
         :param pos_y: y-координата вершины
         :param **kwargs: Аргументы конструктора Node
         """
+        assert id not in self._nodes
         node = Node(self.widget, **kwargs)
         node.setPos(pos_x, pos_y)
         self.widget.scene().addItem(node)
@@ -56,17 +78,28 @@ class GraphModule:
         edge = Edge(self._nodes[id1], self._nodes[id2], self.widget, **kwargs)
         self.widget.scene().addItem(edge)
 
+    def _on_text_link_clicked(self, link):
+        if link[:11] == 'internal://':
+            if re.search(r'close\?id=[0-9]+', link):
+                id = link[re.search(r'id=[0-9]+', link).start()+3:]
+                try:
+                    self.widget.scene().removeItem(self._texts[id])
+                except KeyError:
+                    id = int(id)
+                    self.widget.scene().removeItem(self._texts[id])
+                del self._texts[id]
+
     @property
     def nodes(self):
         """Вернуть список вершин.
         Возможно (TODO), здесь будет реализовано скрытие вершин
         """
-        return self._nodes.values()
+        return list(self._nodes.values())
 
     @property
     def edges(self):
         """Вернуть список ребер"""
-        return self._edges.values()
+        return list(self._edges.values())
 
     def _adjust_scene(self):
         """ Подровнять сцену под вершины """
@@ -74,7 +107,7 @@ class GraphModule:
             self.widget.scene().setSceneRect(-200, -200, 400, 400)
         min_x, min_y, max_x, max_y = sys.maxsize, sys.maxsize, \
             -sys.maxsize, -sys.maxsize
-        for node in self.nodes:
+        for node in self.nodes + list(self._texts.values()):
             min_x = min(node.x(), min_x)
             min_y = min(node.y(), min_y)
             max_x = max(node.x(), max_x)
@@ -92,6 +125,15 @@ class GraphModule:
         self.gravity_timer = QTimer(self.widget)
         self.gravity_timer.start(20)
         self.gravity_timer.timeout.connect(self._process_gravity)
+
+    def do_gravity_ticks(self, ticks):
+        """Выполнить нужное количество тиков расчёта гравитации.
+        Можно использовать для первоначальной стабилизации сцены.
+
+        :param ticks: Количество тиков
+        """
+        for _ in range(ticks):
+            self._process_gravity()
 
     def _process_gravity(self):
         """ Один тик обработки взаимодействия вершин """
@@ -132,8 +174,6 @@ class GraphModule:
                 dy = node.y() - edge.source.y()
             xvel -= dx / weight
             yvel -= dy / weight
-
-        # TODO (?) Силы, отталкивающие вершины от границ
 
         if np.linalg.norm((xvel, yvel)) < 0.2:
             xvel = yvel = 0
