206bc66b9e19a17899e3e79887dcf1b0181ee494
ierikhon <kinglordlear@gmail.com>
2019-04-17 19:31:24 +0300

    Supreme settings getter created

--- src/supremeSettings.py
+++ src/supremeSettings.py
@@ -1,10 +1,19 @@
 class SupremeSettings:
-
-    size_value = 20
-    vertex_weight = 250
+    settings = {
+        'size_value': 20,
+        'vertex_weight': 250,
+        'n': 100
+    }
 
     def __init__(self):
         self.initialise_settings()
+
+    def get_settings(self, setting_names):
+        result = {}
+        for setting in setting_names:
+            if setting in self.settings.keys():
+                result[setting] = self.settings[setting]
+        return result
 
     def initialise_settings(self):
         """TODO вытащить настройки из интерфейса"""
