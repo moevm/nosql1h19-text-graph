0ade7dafb5ff343129b096ae52823b34cad85c2a
ierikhon <kinglordlear@gmail.com>
2019-04-17 19:06:15 +0300

    Supreme settings created

---
+++ src/supremeSettings.py
@@ -0,0 +1,11 @@
+class SupremeSettings:
+
+    size_value = 20
+    vertex_weight = 250
+
+    def __init__(self):
+        self.initialise_settings()
+
+    def initialise_settings(self):
+        """TODO вытащить настройки из интерфейса"""
+        pass
